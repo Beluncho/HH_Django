aba826fd8b4822b08999b46591b2f8178b8cdee4
Beluncho <belov-bk@mail.ru>
2025-03-12 17:51:03 +0300

    add change in admin.py {hhapp and userapp} like in nu25

--- HH/hhapp/admin.py
+++ HH/hhapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Vacancies, Employer
 
+class VacanciesAdmin(admin.ModelAdmin):
+    list_display = ['vac_name','url_vac', 'employer', 'user']
 
-admin.site.register(Vacancies)
+admin.site.register(Vacancies, VacanciesAdmin)
 admin.site.register(Employer)
 
--- HH/userapp/admin.py
+++ HH/userapp/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import WebSiteUser
 
 # Register your models here.
-admin.site.register(WebSiteUser)
+def give_is_employer(modeladmin, request, queryset):
+    queryset.update(is_employer = True)
+
+give_is_employer.short_descripyion = 'mark as employer'
+
+class WebSiteUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'is_employer']
+    actions = [give_is_employer]
+
+
+admin.site.register(WebSiteUser, WebSiteUserAdmin)
